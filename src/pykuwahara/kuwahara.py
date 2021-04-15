correct variance calculation using 2D grayscale image

src/pykuwahara/kuwahara.py
@@ -1,14 +1,23 @@
 import numpy as np
 import cv2
 
-def kuwahara(orig_img, radius, sigma=None, method='mean'):
+
+def kuwahara(orig_img, method='mean', radius=3, sigma=None, grayconv=cv2.COLOR_BGR2GRAY, image_2d=None):
     """
     Get the image filtered with Kuwahara method.
 
     :param orig_img: original numpy image (support multichannel)
     :type orig_img: :class:`numpy.ndarray`
-    :param radius: the window radius (winsize = 2 * radius + 1)
+    :param method: method used to compute the pixels values
+    :type method: "gaussian" | "mean"
+    :param radius: the window radius (`winsize = 2 * radius + 1`)
     :type radius: `int`
+    :param sigma: the sigma used if metod is "gaussian", automatically computed by OpenCV when `None`
+    :type sigma: `float` or `None`
+    :param grayconv: The OpenCV conversion code to extract grayscale image from `orig_img` (default `COLOR_BGR2GRAY`)
+    :type grayconv: `int`
+    :param image_2d: The 1-channel image used to compute the variance, if provided instead of `grayconv`
+    :type image_2d: `numpy.ndarray`
     :returns: the filtered image
     :rtype: `numpy.ndarray`
     """
@@ -29,11 +38,14 @@ def kuwahara(orig_img, radius, sigma=None, method='mean'):
     #       python-kuwahara by Andrew Dussault, 2015 : https://github.com/adussault/python-kuwahara
     #       which it itself based on original Matlab code from Luca Balbi, 2007
 
+    if orig_img.ndim != 2 and orig_img.ndim != 3:
+        raise TypeError("Incorrect number of dimensions (excepted 2 or 3)")
+
     if not isinstance(radius, int):
-        raise TypeError('radius must be int')
+        raise TypeError('`radius` must be int')
 
     if radius < 1:
-        raise ValueError('radius must be greater or equal 1')
+        raise ValueError('`radius` must be greater or equal 1')
 
     if method not in ('mean', 'gaussian'):
         raise NotImplementedError('unsupported method %s' % method)
@@ -43,23 +55,34 @@ def kuwahara(orig_img, radius, sigma=None, method='mean'):
         # then computed by OpenCV as : 0.3 * ((ksize - 1) * 0.5 - 1) + 0.8
         # see: https://docs.opencv.org/master/d4/d86/group__imgproc__filter.html#gac05a120c1ae92a6060dd0db190a61afa
 
-    # convert to float64 if necessary for further math computation
+    # convert to float32 if necessary for further math computation
     image = orig_img.astype(np.float64, copy=False)
 
-    # Create a pixel-by-pixel square of the image
-    squared_img = image ** 2
+    if image_2d is not None:
+        image_2d = image_2d.astype(image.dtype, copy=False)
 
     # preallocate these arrays
-    avgs = np.empty((4, *image.shape))
-    stddevs = np.empty((4, *image.shape))
+    avgs = np.empty((4, *image.shape), dtype=image.dtype)
+    stddevs = np.empty((4, *image.shape[:2]), dtype=image.dtype)
+
+    if image.ndim == 3:
+        if image_2d is None:
+            image_2d = cv2.cvtColor(orig_img, grayconv).astype(image.dtype, copy=False)
+        avgs_2d = np.empty((4, *image.shape[:2]), dtype=image.dtype)
+    elif image.ndim == 2:
+        image_2d = image
+        avgs_2d = avgs
+
+    # Create a pixel-by-pixel square of the image
+    squared_img = image_2d ** 2
 
     if method == 'mean':
-        kxy = np.ones(radius + 1) / (radius + 1)    # kernelX and kernelY (same)
+        kxy = np.ones(radius + 1, dtype=image.dtype) / (radius + 1)    # kernelX and kernelY (same)
     elif method == 'gaussian':
-        kxy = cv2.getGaussianKernel(2 * radius + 1, sigma)
+        kxy = cv2.getGaussianKernel(2 * radius + 1, sigma, ktype=cv2.CV_64F)
         kxy /= kxy[radius:].sum()   # normalize the semi-kernels
-        klr = [kxy[:radius+1], kxy[radius:]]
-        kindexes = [(1, 1), (1, 0), (0, 1), (0, 0)]
+        klr = np.array([kxy[:radius+1], kxy[radius:]])
+        kindexes = [[1, 1], [1, 0], [0, 1], [0, 0]]
 
     # the pixel position for all kernel quadrants
     shift = [(0, 0), (0,  radius), (radius, 0), (radius, radius)]
@@ -69,16 +92,20 @@ def kuwahara(orig_img, radius, sigma=None, method='mean'):
         if method == 'mean':
             kx = ky = kxy
         elif method == 'gaussian':
-            ix, iy = kindexes[k]
-            kx, ky = klr[ix], klr[iy]
+            kx, ky = klr[kindexes[k]]
         cv2.sepFilter2D(image, -1, kx, ky, avgs[k], shift[k])
+        if image.ndim == 3: # else, this is already done...
+            cv2.sepFilter2D(image_2d, -1, kx, ky, avgs_2d[k], shift[k])
         cv2.sepFilter2D(squared_img, -1, kx, ky, stddevs[k], shift[k])
-        stddevs[k] = stddevs[k] - avgs[k] ** 2    # variance on subwindow
+        stddevs[k] = stddevs[k] - avgs_2d[k] ** 2    # compute the final variance on subwindow
 
     # Choice of index with minimum variance
     indices = np.argmin(stddevs, axis=0)
 
     # Building the filtered image
-    filtered = np.take_along_axis(avgs, indices[None,...], 0).reshape(image.shape)
+    if image.ndim == 2:
+        filtered = np.take_along_axis(avgs, indices[None,...], 0).reshape(image.shape)
+    else:   # then avgs.ndim == 4
+        filtered = np.take_along_axis(avgs, indices[None,...,None], 0).reshape(image.shape)
 
     return filtered.astype(orig_img.dtype)